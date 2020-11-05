5d805c565ade64d2a21f7e0fb040a2bfc64f35f8
harmatosbikafing <70636500+harmatosbikafing@users.noreply.github.com>
2020-11-05 11:53:23 +0100

    Few changes, start using Bootstrap

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("nav").style.padding = "30px 10px";
+        document.getElementsByClassName("nav").style.padding = "20px";
         document.getElementById("name").style.fontSize = "25px";
     }
     else {
-        document.getElementById("nav").style.padding = "40px 10px";
+        document.getElementsByClassName("nav").style.padding = "30px";
         document.getElementById("name").style.fontSize = "35px";
     }
 }
