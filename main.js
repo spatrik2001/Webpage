f02cf92077848b79de1e51cb403e944ae1657d66
harmatosbikafing <70636500+harmatosbikafing@users.noreply.github.com>
2020-11-06 13:22:28 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementsByClassName("nav").style.padding = "20px";
+        document.getElementById("nav").style.padding = "20px";
         document.getElementById("name").style.fontSize = "25px";
     }
     else {
-        document.getElementsByClassName("nav").style.padding = "30px";
+        document.getElementById("nav").style.padding = "30px";
         document.getElementById("name").style.fontSize = "35px";
     }
 }
